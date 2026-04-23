f498db420fa0a51ea21aca9683f5d470c4d98aa3
Wederson Ferreira Roberto <emrferramentas@gmail.com>
2026-04-23 17:08:03 -0300

    botão

--- janela.py
+++ janela.py
@@ -1,7 +1,19 @@
 import tkinter as tk
-
+def clicar():
+    print("Play")
 janela = tk.Tk()
 janela.title("WFPLAYER")
 janela.geometry("300x200")
 
+
+botao = tk.Button(
+    janela,
+    text = "PLAY",
+    command=clicar,
+    font=("Arial",16),
+    bg="black",
+    fg="white", 
+    width=5,
+    height=1)
+botao.place(relx=0.5,rely=0.92,anchor="center")    
 janela.mainloop()
